7d0757eff46120a7dd4e1ce8322428fa546bf18a
Dmitry Kazhaev <pset3@yandex.ru>
2024-06-04 12:33:28 +0300

    Fix string format

--- Assets/Scripts/UI/Info/UIInfoView.cs
+++ Assets/Scripts/UI/Info/UIInfoView.cs
@@ -23,9 +23,9 @@ namespace Asteroid
             stringBuilder.Append("Position: ");
             stringBuilder.AppendLine(Game.Player.Model.Position.ToString());
             stringBuilder.Append("Rotation: ");
-            stringBuilder.AppendLine(Game.Player.Model.Rotation.ToString());
+            stringBuilder.AppendLine(Game.Player.Model.Rotation.ToString("0.00"));
             stringBuilder.Append("Speed: ");
-            stringBuilder.AppendLine(Game.Player.Model.Speed.ToString());
+            stringBuilder.AppendLine(Game.Player.Model.Speed.ToString("0.00"));
             stringBuilder.Append("Laser Charge Count: ");
             stringBuilder.AppendLine(Game.Player.Model.LaserCount.ToString());
             stringBuilder.Append("Reload Laser Time: ");
